2ab8a4309ba0adc013ae93d0a6f80110a58805d8
turtles <me@ramon.nyc>
2020-11-18 21:50:35 -0800

    forgot to comment, changed the order of the length prompt to match the class video. added sections for read me.

--- Develop/script.js
+++ Develop/script.js
@@ -2,34 +2,29 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
+  //User selects password length.
+  var length = prompt("Please choose a password character length between 8 and 128.");
+
+  //Check user has selected correct password length.
+  while (length < 8 || length > 129) {
+    var length = prompt("You did not choose an appropriate character length. Please choose a password character length between 8 and 128.");
+  }
+
+  console.log(length);
   
-  //Present Criteria.
-  alert("Please select from the following prompts what you would like your random password to include. Click OK for yes and CANCEL for no.");
-  
-  //Gather password variables.
-  var lower = confirm("Do you want your password to include lowercase characters?");
-  var upper = confirm("Do you want your password to include uppercase characters?");
-  var numeric = confirm("Do you want your password to include numerical characters?");
-  var special = confirm("Do you want your password to include special characters?");
+  //User chooses type of characters to include in generated password.
+  var lower = confirm("Click OK if you want your password to include lowercase characters?");
+  var upper = confirm("Click OK if you your password to include uppercase characters?");
+  var numeric = confirm("Click OK if you your password to include numerical characters?");
+  var special = confirm("Click OK if you your password to include special characters?");
   
   console.log(lower);
   console.log(upper);
   console.log(numeric);
   console.log(special);
 
-  //User selets password length.
-  var length = prompt("Please choose a password character length between 8 and 128.");
-
-  //Check user has selected correct password length.
-  while (length < 8 || length > 129) {
-    var length = prompt("You did not choose an appropriate character length. Please try a password character length between 8 and 128.");
-  }
-  console.log(length);
-
-
-
-  var pw = "ramon";
-  return pw;
+  return length;
   
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# password-generator
+# Usage & Description
+    to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+# Screenshot
+
+
+# Link to deployed application
+
+
+# Credits
+Ramon Torres, Los Anglese California. Columbia Fullstack Bootcamp.
+
+# License
+2020 Ramon, Inc.
